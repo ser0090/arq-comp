b140a1abf70a7d6549f1a50a263b9cff9d656ba7
Martin Alejandro Barrera <martin.barrera@alumnos.unc.edu.ar>
2018-09-19 13:18:53 -0300

    funcionando como modulo combinacional a la entrada - modulo secuencial para cambio de estado

--- ADC-TP2_UART/src/Rx_uart.v
+++ ADC-TP2_UART/src/Rx_uart.v
@@ -30,6 +30,8 @@ always @(posedge i_clk or posedge i_rst) begin
     state <= idle; // cambio a estado inicial
     next_state <= idle;
     time_count <= 5'h0;
+    data_count <= 4'h0;
+    shift_reg <= {NB_BITS{1'b0}};
   end
   else begin
       state <= next_state;
@@ -38,68 +40,68 @@ always @(posedge i_clk or posedge i_rst) begin
 end
 
 
-always @(posedge i_clk) begin
+always @(*) begin
   if (i_rst) begin
     
-    shift_reg <= {NB_BITS{1'b0}}; // inicializa el registro en 0x00
-    data_count <= 4'h0;
-    rx_data <= {NB_BITS{1'b0}};
-    rx_done <= 1'b0;
+    shift_reg = {NB_BITS{1'b0}}; // inicializa el registro en 0x00
+    data_count = 4'h0;
+    rx_data = {NB_BITS{1'b0}};
+    rx_done = 1'b0;
   end
   else begin
   case (state)
     idle: begin
             if(i_rx == 1'b0) begin
-              next_state <= start; //cambio de estado
-              time_count <= 5'h7; // precarga del contador para contar 7
+              next_state = start; //cambio de estado
+              time_count = 5'h7; // precarga del contador para contar 7
               end
             else begin
               next_state = next_state; // queda donde esta
-              time_count <= time_count; // precarga del contador para contar 7
+              time_count = time_count; // precarga del contador para contar 7
 
             end
           end
 
     start: begin
-            data_count <= 0;
+            data_count = 4'h0;
             if(time_count[4] == 1'b1) begin // si desborda el contador
-              time_count [4] <= 1'b0; // reinicio el tiempo
-              next_state <= receiving; //cambio de estado
+              time_count [4] = 1'b0; // reinicio el tiempo
+              next_state = receiving; //cambio de estado
               end
             else begin
-              next_state <= next_state;
+              next_state = next_state;
             end
           end
 
     receiving: begin
                 if(data_count == NB_BITS) begin // si ya recibi todos los bits
-                  time_count [4] <= 1'b0; // reinicio del contador
-                  next_state <= stop; // cambio de estado
+                  time_count [4] = 1'b0; // reinicio del contador
+                  next_state = stop; // cambio de estado
                   end
                 else if(time_count[4] == 1'b1) begin // si aun faltan bits y desbordo el contador
-                  shift_reg <= {i_rx , shift_reg[NB_BITS-1:1]}; // shift al registro y carga de rx en MSB       
-                  data_count <= data_count + 1;
-                  time_count [4] <= 1'b0; // reinicio del contador
+                  shift_reg = {i_rx , shift_reg[NB_BITS-1:1]}; // shift al registro y carga de rx en MSB       
+                  data_count = data_count + 1;
+                  time_count [4] = 1'b0; // reinicio del contador
                   end
                 else begin
-                  next_state <= next_state;
-                  data_count <= data_count ;
+                  next_state = next_state;
+                  data_count = data_count ;
                   end
               end
 
     stop: begin
             if(time_count[4] == 1'b1) begin
               if(i_rx == 1'b1) begin
-                rx_data <= shift_reg;
-                rx_done <= 1'b1;
-                next_state <= idle;
+                rx_data = shift_reg;
+                rx_done = 1'b1;
+                next_state = idle;
                 end
               else begin
-                next_state <= idle;
+                next_state = idle;
                 end
               end  
             else begin
-                  next_state <= next_state;
+                  next_state = next_state;
                 end    
           end
   endcase
