7788e726597470823217e86c6c853c59ec876ea1
tincho <martin.barrera@outlook.com>
2019-05-04 14:02:59 -0300

    comlemento a dos del compilador solucionado

--- mips_final/compiler_utils/compiler_funcs/i_type.py
+++ mips_final/compiler_utils/compiler_funcs/i_type.py
@@ -2,13 +2,28 @@ from . import opcode
 from . import reg_file
 
 def to_16_bit_str(value):
-	value = int(value)
+	value_aux = int(value)
 	string = ''
 	for i in range (0,16):
-		res = '1' if (value % 2) else '0'
-		value = value // 2
+		res = '1' if (value_aux % 2) else '0'
+		value_aux = value_aux // 2
 		string = string+res
-	return string[::-1] #reverse string
+	string = string[::-1] #reverse string
+	return string if int(value) > 0 else complementar(string)
+
+def complementar(binary_string):
+	binary_string = binary_string[::-1] 
+	complemented = ''
+	for i in range(0,len(binary_string)):
+		complemented = complemented + binary_string[i]
+		if binary_string[i] is '1':
+			break;
+
+	for i in range(len(complemented),len(binary_string)):
+		complemented = (complemented + '0') if binary_string[i] is '0' else (complemented + '1')
+
+	return complemented[::-1]
+
 
 def addi(parsed_params):
 	rt = parsed_params.group(2).strip()
