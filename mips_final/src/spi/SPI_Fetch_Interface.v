d9353d36943599c48cb2456d721b59e95d04029d
ser0090 <ser.0090@gmail.com>
2019-05-16 19:27:26 -0300

    cambio de if else if por case para evitar turbiedades cuando sintetiza

--- mips_final/src/spi/SPI_Fetch_Interface.v
+++ mips_final/src/spi/SPI_Fetch_Interface.v
@@ -19,48 +19,60 @@ module SPI_Fetch_Interface#(
 		input									 i_clk,
 		input									 i_rst
 	);
-  /* Estructura de lo recibido desde el micro
-	 * 22: operaciones sobre la memoria ; 1 = write, 0 = nada
-	 * 20-19: contiene un dato: 00 = operar sobre memoria ,  
-	 *													01 = HL_DATA
-	 *													10 = HU_DATA
-	 *													11 = ADDRESS
-	 * 18: unused
-	 * 17-16: selector para extraer datos : 00 = PC
-	 *																			01 = PC_LATCHED
-	 *																			10 = INSTRUCTION_LATCHED
-	 * 15- 0: half-word data				
-	 */
-	reg [RAM_DEPTH-1:0] addr;
-	reg [NB_BITS-1:0]   data;
-	reg [NB_BITS-1:0]   to_SPI;
+   /* Estructura de lo recibido desde el micro
+	  * 22: operaciones sobre la memoria ; 1 = write, 0 = nada
+	  * 20-19: contiene un dato: 00 = operar sobre memoria ,  
+	  *													01 = HL_DATA
+	  *													10 = HU_DATA
+	  *													11 = ADDRESS
+	  * 18: unused
+	  * 17-16: selector para extraer datos : 00 = PC
+	  *																			01 = PC_LATCHED
+	  *																			10 = INSTRUCTION_LATCHED
+	  * 15- 0: half-word data				
+	  */
+	 reg [RAM_DEPTH-1:0]     addr;
+	 reg [NB_BITS-1:0]       data;
+	 reg [NB_BITS-1:0]       to_SPI;
 
-	assign o_addr = i_in_use? addr:i_PC[RAM_DEPTH-1+2:2];
-	assign o_SPI  = to_SPI;
-  assign o_data = data;
-  assign o_wea = i_in_use && i_SPI[22];
+	 assign o_addr = i_in_use? addr:i_PC[RAM_DEPTH-1+2:2];
+	 assign o_SPI  = to_SPI;
+   assign o_data = data;
+   assign o_wea = i_in_use && i_SPI[22];
 
-  localparam WRITE_MEM_OR_NONE = 2'b00;
-  localparam WRITE_DATA_HL     = 2'b01;
-  localparam WRITE_DATA_HU     = 2'b10;
-  localparam WRITE_ADDR        = 2'b11;
+   localparam WRITE_MEM_OR_NONE = 2'b00;
+   localparam WRITE_DATA_HL     = 2'b01;
+   localparam WRITE_DATA_HU     = 2'b10;
+   localparam WRITE_ADDR        = 2'b11;
 
-
-	always @(posedge i_clk) begin
-		if (i_rst) begin
-			addr <= {RAM_DEPTH{1'b0}};
-			data <= {NB_BITS{1'b0}};
-		end
-		else if (i_SPI[20:19] == WRITE_DATA_HL) begin //HL
-			data[15:0]  <= i_SPI[15:0];
-		end
-		else if (i_SPI[20:19] == WRITE_DATA_HU) begin //HU
-			data[31:16] <= i_SPI[15:0];
-		end
-		else if (i_SPI[20:19] == WRITE_ADDR) begin //ADDR
-			addr <= i_SPI[RAM_DEPTH-1:0];
-		end
-	end
+	 always @(posedge i_clk) begin
+		  if (i_rst) begin
+			   addr <= {RAM_DEPTH{1'b0}};
+			   data <= {NB_BITS{1'b0}};
+	    end
+		  else begin
+         case (i_SPI[20:19])
+           WRITE_DATA_HL: begin
+			        data[15:0]  <= i_SPI[15:0];
+			        data[31:16] <= data[31:16];
+			        addr <= addr;
+           end
+		       WRITE_DATA_HU: begin //HU
+			        data[31:16] <= i_SPI[15:0];
+			        data[15:0]  <= data[15:0];
+			        addr <= addr;
+		       end
+		       WRITE_ADDR: begin //ADDR
+			        addr <= i_SPI[RAM_DEPTH-1:0];
+              data <= data;
+		       end
+           default: begin
+			        addr <= addr;
+              data <= data;
+           end
+         endcase // case (i_SPI[20:19])
+      end // else: !if(i_rst)
+	 end // always @ (posedge i_clk)
 
 
 	localparam GET_PC      = 2'b00;
