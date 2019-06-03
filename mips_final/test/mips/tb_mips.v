252db522ecc2b0286e324397d36633a4f11e0ace
ser0090 <ser.0090@gmail.com>
2019-06-03 12:02:25 -0300

    test de instruccion halt, salida por gpio

--- mips_final/test/mips/tb_mips.v
+++ mips_final/test/mips/tb_mips.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 ///  SER0090
-//`include "/home/ssulca/arq-comp/mips_final/include/include.v"  //Comentar
-`include "/home/sergio/arq-comp/mips_final/include/include.v"  //Comentar
+`include "/home/ssulca/arq-comp/mips_final/include/include.v"  //Comentar
+//`include "/home/sergio/arq-comp/mips_final/include/include.v"  //Comentar
 
 //`include "/home/tincho/Documentos/ADC/arq-comp/mips_final/include/include.v" //Comentar
 module tb_mips();
@@ -19,17 +19,15 @@ module tb_mips();
    reg                i_valid;
    reg                i_clk;
    reg                i_rst;
-   wire [NB_BITS-1:0] o_data;
    wire [NB_BITS-1:0] o_MISO;
-   wire [5:0]         o_operation;
-   wire [5:0]         o_function;
+   wire               o_halt;
 
    initial begin
-      i_clk = 1'b1;
-      i_rst = 1'b1;
+      i_clk      = 1'b1;
+      i_rst      = 1'b1;
       i_continue = 1'b1;
       i_valid    = 1'b0;
- 
+
       #4.9 i_rst = 1'b0;
       /*i_continue = 1'b0;
       #10 i_valid    = 1'b1;
@@ -61,6 +59,7 @@ module tb_mips();
       //.o_operation (o_operation),
       //.o_function  (o_function),
       .o_MISO      (o_MISO),
+      .o_halt      (o_halt),
       .i_valid     (i_valid),
       .i_continue  (i_continue),
       .i_clk       (i_clk),
